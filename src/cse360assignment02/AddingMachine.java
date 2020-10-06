da6cca85845732ff7b46579c8b568888bef81c89
Jason Reyes <jdreyes6@asu.edu>
2020-10-05 18:25:12 -0700

    its has ben done v1

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+/**
+ * class representation of calcuation
+ */
+public class AddingMachine {
+    private int total;
+
+    /**
+     * constructor initializes adding machine
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     *
+     * method that grabs total
+     * @return total
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * addition to total amount
+     * @param value number to addition
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * subtraction oif total amount
+     * @param value number to subtract
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * object to string
+     * @return simple mathematic arguement print out
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * reset int total
+     */
+    public void clear() {
+    }
+}
